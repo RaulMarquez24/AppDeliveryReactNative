87aa1b6da76f7583d66b26133fcbb835bc7ef224
Raúl Márquez <raulmu785@gmail.com>
2023-07-03 14:05:55 +0200

    Inicio pantalla 1 + assests

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/chef.jpg')} style={styles.imageBackground}/>
+      
+      <View style={styles.form}>
+
+      </View>
+      
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -13,8 +18,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+
+  imageBackground:{
+    width:'100%',
+    height:'100%',
+    opacity: 0.7,
+    bottom: '30%',
+  },
+
+  form:{
+    width: '100%',
+    height: '40%',
+    backgroundColor: '#fff',
+    position: 'absolute',
+    bottom:0,
+    borderTopLeftRadius:40,
+    borderTopRightRadius:40,
+  }
 });
