129b4dd5d9ed679ba3a02c483917e0968039830b
Raúl Márquez <raulmu785@gmail.com>
2023-07-11 14:34:50 +0200

    view crear categoria

--- src/Presentation/views/admin/category/create/CategoryCreate.tsx
+++ src/Presentation/views/admin/category/create/CategoryCreate.tsx
@@ -1,10 +1,50 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import styles from './Styles'
+import useViewModel from './ViewModel'
+import { CustomTextInput } from '../../../../components/CustomTextInput'
+import { RoundedButton } from '../../../../components/RoundedButton'
 
 export const AdminCategoryCreateScreen = () => {
+
+    const { name, description, image, onChange } = useViewModel();
+
     return (
-        <View>
-            <Text>AdminCategoryCreateScreen</Text>
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.imageContaine}>
+                <Image
+                    style={styles.image}
+                    source={require('../../../../../../assets/image_new.png')}
+                />
+            </TouchableOpacity>
+
+            <View style={styles.form}>
+                <CustomTextInput
+                    placeholder='Nombre de la categoria'
+                    image={require('../../../../../../assets/categories.png')}
+                    keyboardType='default'
+                    property='name'
+                    value={name}
+                    onChangeText={onChange}
+                />
+
+                <CustomTextInput
+                    placeholder='Descripción'
+                    image={require('../../../../../../assets/description.png')}
+                    keyboardType='default'
+                    property='description'
+                    value={description}
+                    onChangeText={onChange}
+                />
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <RoundedButton
+                    text='CREAR CATEGORIA'
+                    onPress={() => { }}
+                />
+            </View>
+
         </View>
     )
 }
