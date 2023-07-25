ca10e07cb65fb5eec4c9a1d7bfb3fd658dec3101
Raúl Márquez <raulmu785@gmail.com>
2023-07-25 21:33:45 +0200

    Install GoogleMaps Expo

---
+++ src/Presentation/views/client/address/map/Styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { MyColors } from '../../../../theme/AppTheme';
+
+const ClientAddressMapStyles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+
+});
+
+export default ClientAddressMapStyles;
